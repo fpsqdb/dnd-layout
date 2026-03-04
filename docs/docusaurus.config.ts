2306fbd05371e6db74457f536d342e7bc2a1d8e2
fpsqdb <fpsqdb@gmail.com>
2026-03-04 17:08:25 +0800

    chore: add optionalDependencies and fix docusaurus baseUrl

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
     },
 
     // Set the production url of your site here
-    url: "https://your-docusaurus-site.example.com",
+    url: "https://dnd-layout.js.org",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: "/",
+    baseUrl: "/dnd-layout/",
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
