0f08300694e70934e83d573b3c5e8cedaea05606
fpsqdb <fpsqdb@gmail.com>
2026-03-25 16:17:26 +0800

    chore: upgrade deps.

--- docs/src/pages/_index.tsx
+++ docs/src/pages/_index.tsx
@@ -1,6 +1,5 @@
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 import Layout from '@theme/Layout';
 import clsx from 'clsx';
@@ -37,7 +36,6 @@ export default function Home(): ReactNode {
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
       </main>
     </Layout>
   );
